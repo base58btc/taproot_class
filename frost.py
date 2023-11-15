b915657d9363e7fbaebaa805292032b80034476f
niftynei <niftynei@gmail.com>
2023-11-15 17:03:54 -0600

    add FROST example, etc

--- frost.py
+++ frost.py
@@ -46,7 +46,7 @@ class FROST:
             # Y
             self.public_key = None
 
-        def init_keygen(self, privkey_int=None):
+        def init_keygen(self):
             Q = FROST.secp256k1.Q
             G = FROST.secp256k1.G()
             # 1. Generate polynomial with random coefficients, and with degree
@@ -54,12 +54,6 @@ class FROST:
             #
             # (a_i_0, . . ., a_i_(t - 1)) ⭠ $ ℤ_q
             self.coefficients = [secrets.randbits(256) % Q for _ in range(self.threshold)]
-
-            if privkey_int:
-                assert privkey_int < Q
-                # b58 school: set our privkey!
-                self.coefficients[0] = privkey_int
-
             # 2. Compute proof of knowledge of secret a_i_0.
             #
             # k ⭠ ℤ_q
@@ -67,7 +61,7 @@ class FROST:
             # R_i = g^k
             nonce_commitment = nonce * G
             # i
-            index_byte = int.to_bytes(self.index, 1, 'big')
+            index_byte = self.index.to_bytes(1, 'big')
             # 𝚽
             context_bytes = self.CONTEXT
             # g^a_i_0
@@ -97,7 +91,7 @@ class FROST:
         def verify_proof_of_knowledge(self, proof, secret_commitment, index):
             G = FROST.secp256k1.G()
             # l
-            index_byte = int.to_bytes(index, 1, 'big')
+            index_byte = index.to_bytes(1, 'big')
             # 𝚽
             context_bytes = self.CONTEXT
             # g^a_l_0
@@ -195,15 +189,22 @@ class FROST:
             first_nonce = nonce_pair[0]
             # e_i
             second_nonce = nonce_pair[1]
+            # Negate d_i and e_i if R is odd
+            if group_commitment.y % 2 != 0:
+                first_nonce = FROST.secp256k1.Q - first_nonce
+                second_nonce = FROST.secp256k1.Q - second_nonce
             # p_i = H_1(i, m, B), i ∈ S
             binding_value = FROST.Aggregator.binding_value(self.index, message, nonce_commitment_pairs, participant_indexes)
             # λ_i
             lagrange_coefficient = self.lagrange_coefficient(participant_indexes)
             # s_i
             aggregate_share = self.aggregate_share
+            # Negate s_i if Y is odd
+            if self.public_key.y % 2 != 0:
+                aggregate_share = FROST.secp256k1.Q - aggregate_share
 
             # z_i = d_i + (e_i * p_i) + λ_i * s_i * c
-            return first_nonce + (second_nonce * binding_value) + lagrange_coefficient * aggregate_share * challenge_hash
+            return (first_nonce + (second_nonce * binding_value) + lagrange_coefficient * aggregate_share * challenge_hash) % FROST.secp256k1.Q
 
     class Aggregator:
         """Class representing the signature aggregator."""
@@ -239,7 +240,7 @@ class FROST:
         def binding_value(self, index, message, nonce_commitment_pairs, participant_indexes):
             binding_value = sha256()
             # l
-            index_byte = int.to_bytes(index, 1, 'big')
+            index_byte = index.to_bytes(1, 'big')
             # B
             nonce_commitment_pairs_bytes = []
             for index in participant_indexes:
@@ -259,13 +260,16 @@ class FROST:
         @classmethod
         def challenge_hash(self, nonce_commitment, public_key, message):
             # c = H_2(R, Y, m)
+            tag_hash = sha256(b'BIP0340/challenge').digest()
             challenge_hash = sha256()
-            challenge_hash.update(nonce_commitment.sec_serialize())
-            challenge_hash.update(public_key.sec_serialize())
+            challenge_hash.update(tag_hash)
+            challenge_hash.update(tag_hash)
+            challenge_hash.update(nonce_commitment.xonly_serialize())
+            challenge_hash.update(public_key.xonly_serialize())
             challenge_hash.update(message)
             challenge_hash_bytes = challenge_hash.digest()
 
-            return int.from_bytes(challenge_hash_bytes, 'big')
+            return int.from_bytes(challenge_hash_bytes, 'big') % FROST.secp256k1.Q
 
         def signing_inputs(self):
             # B = ⟨(i, D_i, E_i)⟩_i∈S
@@ -287,7 +291,12 @@ class FROST:
             challenge_hash = self.challenge_hash(group_commitment, self.public_key, self.message)
             # TODO: verify each signature share
             # σ = (R, z)
-            return group_commitment, sum(signature_shares)
+            nonce_commitment = group_commitment.xonly_serialize()
+            z = (
+                sum(signature_shares) % FROST.secp256k1.Q
+            ).to_bytes(32, 'big')
+
+            return (nonce_commitment + z).hex()
 
     class Point:
         """Class representing an elliptic curve point."""
@@ -305,6 +314,7 @@ class FROST:
             x = int.from_bytes(x_bytes, 'big')
             y_squared = (pow(x, 3, P) + 7) % P
             y = pow(y_squared, (P + 1) // 4, P)
+
             if y % 2 == 0:
                 even_y = y
                 odd_y = (P - y) % P
@@ -318,7 +328,23 @@ class FROST:
         def sec_serialize(self):
             prefix = b'\x02' if self.y % 2 == 0 else b'\x03'
 
-            return prefix + int.to_bytes(self.x, 32, 'big')
+            return prefix + self.x.to_bytes(32, 'big')
+
+        @classmethod
+        def xonly_deserialize(cls, hex_public_key):
+            P = FROST.secp256k1.P
+            hex_bytes = bytes.fromhex(hex_public_key)
+            x = int.from_bytes(hex_bytes, 'big')
+            y_squared = (pow(x, 3, P) + 7) % P
+            y = pow(y_squared, (P + 1) // 4, P)
+
+            if y % 2 != 0:
+                y = (P - y) % P
+
+            return cls(x, y)
+
+        def xonly_serialize(self):
+            return self.x.to_bytes(32, 'big')
 
         # point at infinity
         def is_zero(self):
@@ -521,11 +547,22 @@ class Tests(unittest.TestCase):
         s2 = p2.sign(message, nonce_commitment_pairs, participant_indexes)
 
         # σ = (R, z)
-        nonce_commitment, s = agg.signature([s1, s2])
+        sig = agg.signature([s1, s2])
+        sig_bytes = bytes.fromhex(sig)
+        nonce_commitment = FROST.Point.xonly_deserialize(sig_bytes[0:32].hex())
+        z = int.from_bytes(sig_bytes[32:64], 'big')
 
         # verify
         G = FROST.secp256k1.G()
         # c = H_2(R, Y, m)
         challenge_hash = FROST.Aggregator.challenge_hash(nonce_commitment, pk, msg)
+        # Negate Y if Y.y is odd
+        if pk.y % 2 != 0:
+            pk = -pk
+
         # R ≟ g^z * Y^-c
-        self.assertTrue(nonce_commitment == (s * G) + (FROST.secp256k1.Q - challenge_hash) * pk)
+        self.assertTrue(nonce_commitment == (z * G) + (FROST.secp256k1.Q - challenge_hash) * pk)
+
+
+if __name__ == '__main__':
+    unittest.main()
